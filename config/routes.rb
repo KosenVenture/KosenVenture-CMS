80bd7eab8de47c47b1854074e3e272e21b414f1c
Hayato OKUMOTO <falcon@falconsrv.net>
2013-04-04 18:35:07 +0900

    Add AdminController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 KvpCms::Application.routes.draw do
   # 管理画面
+  get '/admin' => 'admin#dashboard', as: 'admin'
   scope 'admin', module: :admin do
     resources :pages
     resources :page_categories
