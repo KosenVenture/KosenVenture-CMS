28289451b1eb889b01d6b9eb075094b0e29cb788
Hayato OKUMOTO <falcon@falconsrv.net>
2013-05-04 17:33:05 +0900

    Change test mailaddress for development

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,6 @@ module KvpCms
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    ADMIN_EMAIL = 'ml@kosen-venture.com'
+    ADMIN_EMAIL = (Rails.env.production? ? 'ml@kosen-venture.com' : 'hayato.okumoto@kosen-venture.com')
   end
 end
