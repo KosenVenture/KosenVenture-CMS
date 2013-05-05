1250dbe85f66612f342933373ae8668956f3fb41
Hayato OKUMOTO <falcon@falconsrv.net>
2013-05-05 23:00:38 +0900

    Add BlogPosts controller

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+class BlogPostsController < ApplicationController
+  layout 'kvp'
+
+  def index
+    @posts = BlogPost.published.newest_published_order
+  end
+
+  def show
+    @post = BlogPost.includes(:author, :category).find(params[:id])
+
+    @page_title = @post.title
+
+    @fb_ogp = {
+      title: @page_title ? "#{@page_title}｜#{@site_config.title}" : @site_config.title,
+      url: news_path(@post),
+      description: @post.intro
+    } unless @post.intro.blank?
+  end
+end
